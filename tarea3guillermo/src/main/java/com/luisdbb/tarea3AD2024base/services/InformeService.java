8ccd9f74fbc752febbfdb0de42b260a0c4635502
guillermomf38 <157827456+guillermomf38@users.noreply.github.com>
2026-06-02 16:46:03 +0200

    Add files via upload

---
+++ tarea3guillermo/src/main/java/com/luisdbb/tarea3AD2024base/services/InformeService.java
@@ -0,0 +1,13 @@
+/**
+ * Clase InformeService.java
+ * 
+ * @author Guillermo Martin Fueyo
+ * @version 1.0
+ */
+
+package com.luisdbb.tarea3AD2024base.services;
+
+public class InformeService {
+
+}
+
